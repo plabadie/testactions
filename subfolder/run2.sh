5885cff6023ad851d372624455c397bfe76c50af
Patrick Labadie <patrick.labadie@sap.com>
2023-04-13 17:30:21 -0400

    trace env var

--- subfolder/run2.sh
+++ subfolder/run2.sh
@@ -1,5 +1,5 @@
 echo pausing for $1 seconds before exiting with code $2, additional param $3
-
+echo "default env var GITHUB_HEAD_REF = ${GITHUB_HEAD_REF}"
 source ./subfolder/common2.sh
 
 commonFunc
